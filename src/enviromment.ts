594b5a36f74d89befe60bf3d04a368e048f572de
Rafael Oliveira de Souza <rafaelsoec@gmail.com>
2024-10-08 18:13:50 -0300

    Ajustando components

--- src/enviromment.ts
+++ src/enviromment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     email: 'extracleanlimpezaprofissional@gmail.com',
-//     apiUrl: 'https://limpezaextraclean.com',
-    apiUrl: 'http://localhost:8080'
+    apiUrl: 'https://limpezaextraclean.com',
+    // apiUrl: 'http://localhost:8080'
 };
