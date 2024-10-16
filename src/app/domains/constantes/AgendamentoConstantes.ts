eee517b07fe0d699cc2043298e101c4add86dd7b
Rafael Oliveira de Souza <rafaelsoec@gmail.com>
2024-10-16 10:01:41 -0300

    Ajuste finos

--- src/app/domains/constantes/AgendamentoConstantes.ts
+++ src/app/domains/constantes/AgendamentoConstantes.ts
@@ -16,6 +16,7 @@ export class AgendamentoConstantes {
 
     public static VALORES_HORAS: { id: HorasEnum, valor: number, descricao: string, numProfissionais: number }[] = [
         { id: HorasEnum.HORAS_4, valor: 125, descricao: '4 Horas - 1 Profissional', numProfissionais: 1 },
+        { id: HorasEnum.HORAS_6, valor: 160, descricao: '6 Horas - 1 Profissional', numProfissionais: 1 },
         { id: HorasEnum.HORAS_8, valor: 190, descricao: '8 Horas - 1 Profissional', numProfissionais: 1 },
         { id: HorasEnum.HORAS_16, valor: 360, descricao: '8 Horas - 2 Profissional', numProfissionais: 2 },
         { id: HorasEnum.HORAS_24, valor: 495, descricao: '8 Horas - 3 Profissional', numProfissionais: 3 },
@@ -87,6 +88,9 @@ export class AgendamentoConstantes {
 
         if (valorHoras == HorasEnum.HORAS_4) {
             info.metragem = 50;
+            info.numProfissionais = 1;
+        } else if (valorHoras == HorasEnum.HORAS_6) {
+            info.metragem = 80;
             info.numProfissionais = 1;
         } else if (valorHoras == HorasEnum.HORAS_8) {
             info.metragem = 120;
