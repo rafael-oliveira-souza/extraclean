85b15db8a59e065aa98ead0a5625bb20e99828a4
Rafael Oliveira de Souza <rafaelsoec@gmail.com>
2024-11-05 01:09:23 -0300

    Ajuste finos

--- src/app/components/gerador-contracheque/gerador-contracheque.component.ts
+++ src/app/components/gerador-contracheque/gerador-contracheque.component.ts
@@ -9,6 +9,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MoedaPipe } from '../../pipes/moeda.pipe';
 import { ProfissionalService } from '../../services/profissional.service';
 import { CalculoFuncionarioDTO } from '../../domains/dtos/CalculoFuncionarioDTO';
+import { DateUtils } from '../../utils/DateUtils';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-gerador-contracheque',
@@ -20,6 +22,7 @@ import { CalculoFuncionarioDTO } from '../../domains/dtos/CalculoFuncionarioDTO'
     MatSelectModule,
     MatDatepickerModule,
     MatInputModule,
+    MatCheckboxModule,
     MoedaPipe
   ],
   templateUrl: './gerador-contracheque.component.html',
@@ -34,13 +37,15 @@ export class GeradorContrachequeComponent implements OnInit {
   public periodo: number = this.hoje.getMonth();
   public horasExtras: number = 0;
   public salarioBase: number = 1450;
-  public valorHorasExtras: number = 1450;
   public planoSaude: number = 0;
+  public numFeriados: number = 0;
   public valeTransporte: number = 405;
   public valeAlimentacao: number = 195;
   public profissionalSelecionado!: ProfissionalDTO;
   public adicionalExtra = 1; // Adicional de 50% para horas extras durante a semana
-
+  public horasMensais: number = this.calcularHorasTotais();
+  public horasTrabalhadas: number = this.horasMensais;
+  public ignorarDescontos: boolean = false;
 
   constructor(public profissionalService: ProfissionalService) { }
 
@@ -57,48 +62,72 @@ export class GeradorContrachequeComponent implements OnInit {
   }
 
   public calcularTotais(salarioBase: number): number {
-    let total = this.calcularINSS(salarioBase);
+    let total = (salarioBase * this.horasTrabalhadas) / this.horasMensais;
+    total += this.calcularINSS(salarioBase);
     total += this.calcularIRRF(salarioBase);
     total += this.calcularHorasExtras(salarioBase, this.horasExtras);
     total += this.planoSaude;
     total += this.valeAlimentacao;
     total += this.valeTransporte;
-    total += this.valorHorasExtras;
 
     return total;
+  }
+
+  public calcularHorasTotais(): number {
+    let data = this.hoje;
+    data.setMonth(this.periodo);
+
+    let total = 0;
+    let datas: Date[] = DateUtils.datesInMonth(data);
+    datas.forEach(data => {
+      const dayOfWeek = DateUtils.toMoment(data).day();// 0 = Domingo, 6 = Sábado
+      if (dayOfWeek === 6) {
+        total += 4;
+      } else if (dayOfWeek === 0) {
+        total += 0;
+      } else {
+        total += 8;
+      }
+    });
+
+    this.horasMensais = total - (this.numFeriados * 8);
+    return this.horasMensais;
   }
 
   public calcularINSS(salarioBase: number): number {
     let inss: number = 0;
 
-    if (salarioBase <= 1412.00) {
-      inss = salarioBase * 0.075;  // 7.5%
-    } else if (salarioBase <= 2666.68) {
-      inss = salarioBase * 0.09;   // 9%
-    } else if (salarioBase <= 4000.03) {
-      inss = salarioBase * 0.12;   // 12%
-    } else if (salarioBase <= 7786.49) {
-      inss = salarioBase * 0.14;   // 14%
+    if (!this.ignorarDescontos) {
+      if (salarioBase <= 1412.00) {
+        inss = salarioBase * 0.075;  // 7.5%
+      } else if (salarioBase <= 2666.68) {
+        inss = salarioBase * 0.09;   // 9%
+      } else if (salarioBase <= 4000.03) {
+        inss = salarioBase * 0.12;   // 12%
+      } else if (salarioBase <= 7786.49) {
+        inss = salarioBase * 0.14;   // 14%
+      }
     }
-
     return inss;
   }
 
   // Função para calcular o IRRF
   public calcularIRRF(salarioBase: number): number {
-    const baseIRRF = salarioBase - this.calcularINSS(salarioBase); // Salário após o desconto do INSS
     let irrf = 0;
+    if (!this.ignorarDescontos) {
+      const baseIRRF = salarioBase - this.calcularINSS(salarioBase); // Salário após o desconto do INSS
 
-    if (baseIRRF <= 1903.98) {
-      irrf = 0; // Isento
-    } else if (baseIRRF <= 2826.65) {
-      irrf = (baseIRRF - 1903.98) * 0.075 - 142.80; // Dedução de R$ 142,80
-    } else if (baseIRRF <= 3751.05) {
-      irrf = (baseIRRF - 2826.65) * 0.15 - 354.80; // Dedução de R$ 354,80
-    } else if (baseIRRF <= 4664.68) {
-      irrf = (baseIRRF - 3751.05) * 0.225 - 636.13; // Dedução de R$ 636,13
-    } else {
-      irrf = (baseIRRF - 4664.68) * 0.275 - 869.36; // Dedução de R$ 869,36
+      if (baseIRRF <= 1903.98) {
+        irrf = 0; // Isento
+      } else if (baseIRRF <= 2826.65) {
+        irrf = (baseIRRF - 1903.98) * 0.075 - 142.80; // Dedução de R$ 142,80
+      } else if (baseIRRF <= 3751.05) {
+        irrf = (baseIRRF - 2826.65) * 0.15 - 354.80; // Dedução de R$ 354,80
+      } else if (baseIRRF <= 4664.68) {
+        irrf = (baseIRRF - 3751.05) * 0.225 - 636.13; // Dedução de R$ 636,13
+      } else {
+        irrf = (baseIRRF - 4664.68) * 0.275 - 869.36; // Dedução de R$ 869,36
+      }
     }
 
     // Certificar que o valor do IRRF não seja negativo
@@ -120,7 +149,7 @@ export class GeradorContrachequeComponent implements OnInit {
   }
 
   public calcularHoraNormal(salarioBase: number): number {
-    const horasTrabalhadasPorMes = 220; // Considerando 44 horas semanais (5 dias de 8 horas)
+    const horasTrabalhadasPorMes = this.calcularHorasTotais(); // Considerando 44 horas semanais (5 dias de 8 horas)
     return salarioBase / horasTrabalhadasPorMes;
   }
 
