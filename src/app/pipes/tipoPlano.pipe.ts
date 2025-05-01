431e4d06bd2e5a7b5d84727bfb822845615f89fe
Rafael Oliveira de Souza <rafaelsoec@gmail.com>
2025-05-01 20:32:53 -0300

    Criacao de aba de planos

--- src/app/pipes/tipoPlano.pipe.ts
+++ src/app/pipes/tipoPlano.pipe.ts
@@ -13,11 +13,15 @@ export class TipoPlanoPipe implements PipeTransform {
         return "2 Diárias";
       case "1":
         return "2 Diárias";
+      case "DIARIA_2":
+        return "2 Diárias";
       case TipoPlanoEnum.DIARIA_2:
         return "2 Diárias";
       case 2:
         return "4 Diárias";
       case "2":
+        return "4 Diárias";
+      case "DIARIA_4":
         return "4 Diárias";
       case TipoPlanoEnum.DIARIA_4:
         return "4 Diárias";
@@ -25,17 +29,23 @@ export class TipoPlanoPipe implements PipeTransform {
         return "6 Diárias";
       case "3":
         return "6 Diárias";
+      case "DIARIA_6":
+        return "6 Diárias";
       case TipoPlanoEnum.DIARIA_6:
         return "6 Diárias";
       case 4:
         return "8 Diárias";
       case "4":
         return "8 Diárias";
+      case "DIARIA_8":
+        return "8 Diárias";
       case TipoPlanoEnum.DIARIA_8:
         return "8 Diárias";
       case 5:
         return "10 Diárias";
       case "5":
+        return "10 Diárias";
+      case "DIARIA_10":
         return "10 Diárias";
       case TipoPlanoEnum.DIARIA_10:
         return "10 Diárias";
